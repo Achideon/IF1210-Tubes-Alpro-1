dcc39fd21f1270d3a3cfda2289508fa1322e6e99
Brain <13524146@std.stei.itb.ac.id>
2025-05-10 20:44:13 +0700

    Feat: menambahkan fitur F02,F10,danF18

---
+++ src/fitur/fitur-021018.h
@@ -0,0 +1,23 @@
+#ifndef fitur_H
+#define fitur_H
+
+#include<stdio.h>
+#include<string.h>
+#include "../adt/boolean.h"
+#include "../adt/user-list.h"
+#include "../adt/matrix-denah.h"
+#include "../utils/writeFile.h"
+
+void regist(ListUser *l, int currentId);
+/*F02 : Prosedur untuk melakukan registrasi bagi pasien yang belum memiliki akun. Hanya bisa dijalankan jika belum login dan username bersifat case-insensitive.*/
+
+void addDoctor(ListUser *l, int currentId);
+/*F10 : Prosedur yang hanya bisa dijalankan oleh Manager dan berfungsi untuk menambah dokter.*/
+
+void assignDoctor(ListUser *l, int currentId, Matrix *M);
+/*F10 : Prosedur untuk assign dokter ke ruangan.*/
+
+void ext(boolean *kondisi, ListUser l,Matrix m);
+/*F18 : Prosedur untuk menghentikan program yang sedang berlangsung. Kemudian, akan ada opsi untuk dipilih antara save semua progress yang ada atau tidak save.*/
+
+#endif

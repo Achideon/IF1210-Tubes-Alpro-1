8a2b4ec6a857f39b2ae6b1ade6a2c6d66377d389
Syaqina Octavia Rizha <syaqinaoctavia@gmail.com>
2025-05-10 20:34:10 +0700

    style: menambahkan komentar terkait deskripsi fitur

--- src/fitur/fitur-01030405.h
+++ src/fitur/fitur-01030405.h
@@ -8,7 +8,28 @@
 
 #include "../adt/user-list.h"
 
-// gcc user-list.c fitur-01030405.c -o p
+/*
+login
+    - Meminta username dan password
+    - Mengecek apakah username dan password valid
+    - Jika valid, set currentID ke ID user yang login
+    - Jika tidak valid, tampilkan pesan error
+logout
+    - Mengubah currentID menjadi MARK_INT
+lupaPassword
+    - Meminta username dan kode unik
+    - Mengecek apakah username valid
+    - Jika valid, lakukan RLE pada username
+    - Jika kode unik sesuai dengan hasil RLE, minta password baru
+    - Set password baru untuk user yang bersangkutan
+    - Tampilkan pesan sukses
+menuHelp
+    - Menampilkan menu bantuan sesuai dengan role yang sedang login
+    - Jika belum login, tampilkan menu login dan register
+    - Jika sudah login, tampilkan menu sesuai dengan role
+    - Tampilkan footnote tentang cara menggunakan aplikasi
+*/
+
 void login(ListUser *l, int *currentID);
 
 void logout(ListUser *l, int *currentID);

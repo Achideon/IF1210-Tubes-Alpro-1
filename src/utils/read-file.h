b5d4b32f9cd2f0d36c40929f97563183f1c85b71
13524110 <13524110@mahasiswa.itb.ac.id>
2025-05-10 20:29:31 +0700

    feat: menambahkan read-file
    penambahan read-file untuk membaca file eksternal

---
+++ src/utils/read-file.h
@@ -0,0 +1,43 @@
+/* SYSTEM PEMBACAAN FILE */
+/* Berisi definisi dan primitif pemrosesan pembacaan file secara sekuensial */
+/* Setelah dibaca, file akan menyimpan value ke database sederhana (berupa list dari struct) */
+
+#ifndef READFILE_H
+#define READFILE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include "boolean.h"
+#include "../adt/user-list.h"
+#include "../adt/matrix-denah.h"
+#include "../adt/queue.h"
+// #include "matrix-denah.h"
+// #include "queue.h"
+
+#define MAX_LINE_LENGTH 1024
+
+#define MAX_USERNAME_LENGTH 40
+#define MAX_PASSWORD_LENGTH 40
+
+// Buat skema pembacaan .txt dan .CSV, value akan disimpan dan diubah 
+// menjadi ListUser berdasarkan definisi di file "user-list.h"
+
+/* ********** OPERASI PEMBACAAN FILE ********** */
+
+void readFileUser(ListUser *l);
+// Memindahkan data dari File User ke databaseUser sementara
+
+void readConfig(Matrix *M);
+// Memindahkan data dari File Config ke databaseMatriks
+
+/* ********** OPERASI PENDUKUNG PEMBACAAN FILE ********** */
+
+void parsing(char *input, char *format, int dataCount, ...);
+// Untuk melakukan parsing tiap pembacaan line CSV
+
+void readDigits(FILE *input, int *number, int *chr);
+// Untuk melakukan parsing terhadap tiap digit pada txt
+
+#endif

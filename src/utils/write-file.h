a37bbbe01085b3c7b4bb3ffa41564b76092404c2
Ishakpln <isaacnapz777999@gmail.com>
2025-05-27 12:31:12 +0700

    fix: penggantian nama fungsi

--- src/utils/write-file.h
+++ src/utils/write-file.h
@@ -6,10 +6,10 @@
 #include "../adt/user-list.h"
 #include "../adt/matrix-denah.h"
 
-void writeFile_user(ListUser l, char* filepath, boolean *status);
+void writeFileUser(ListUser l, char* filepath, boolean *status);
 /*Menulis file user.csv*/
 
-void writeFile_config(Matrix *M, char* filepath, boolean *status);
+void writeFileConfig(Matrix *M, char* filepath, boolean *status);
 /*Menulis file config.txt*/
 
 #endif

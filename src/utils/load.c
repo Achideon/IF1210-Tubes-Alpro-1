8afdcfe6fab1a1df98af64c97a08665c38588f2d
Syaqina Octavia Rizha <syaqinaoctavia@gmail.com>
2025-05-29 22:02:03 +0700

    fix: memperbaiki fungsi load

--- src/utils/load.c
+++ src/utils/load.c
@@ -2,13 +2,13 @@
 
 int isFolderExist(char *path){
     struct stat st;
-    if(stat(path, &st) == 0){
-        return S_ISDIR(st.st_mode);
+    if(stat(*path, &st) == 0 && S_ISDIR(st.st_mode)){
+        return 1;
     }return 0;
 }
 
 void load(Matrix *M, ListUser *l, ListInventory *li, boolean *status){
-    char folder_name[30];
+    char folder_name[30] = "";
     char path[50];
     char user_path[100];
     char config_path[100];
@@ -20,21 +20,19 @@ void load(Matrix *M, ListUser *l, ListInventory *li, boolean *status){
     // Gabungkan base_path dan folder_name
     snprintf(path, sizeof(path), "%s%s", base_path, folder_name);
     
-    while(!strcmp(path, base_path) || !isFolderExist(path)){
-        scanf("%49s", folder_name);
-        snprintf(path, sizeof(path), "%s%s/", base_path, folder_name);
-        if(!isFolderExist(path)){
+    if(!strcmp(path, base_path) || !isFolderExist(&path)){
+        if(!isFolderExist(&path)){
             printf("folder %s tidak ditemukan", folder_name);
         }else if(!strcmp(path, base_path)){
             printf("Tidak ada nama folder yang diberikan!");
-            printf("Usage : ./make <<nama_folder>>");
+            printf("Usage : ./final_program <<nama_folder>>");
         }
         *status = false;
     }
-    if(isFolderExist(path)){
+    if(isFolderExist(&path)){
         snprintf(user_path, sizeof(user_path), "%s%s", path, user_file);
-        readFileUser(l, user_path);
+        readFileUser(l, &user_path);
         snprintf(config_path, sizeof(config_path), "%s%s", path, config_file);
-        readConfig(l, config_path, li);
+        readConfig(l, &config_path, li);
     }
 }

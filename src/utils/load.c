b0bc89303eb8eb60647852fe5ee47ae1d98dbfdf
SyaqinaOctavia <syaqinaoctavia@gmail.com>
2025-05-30 00:08:37 +0700

    fix: memperbaiki fungsi load

--- src/utils/load.c
+++ src/utils/load.c
@@ -2,7 +2,7 @@
 
 int isFolderExist(char *path){
     struct stat st;
-    if(stat(*path, &st) == 0 && S_ISDIR(st.st_mode)){
+    if(stat(path, &st) == 0 && S_ISDIR(st.st_mode)){
         return 1;
     }return 0;
 }
@@ -11,14 +11,14 @@ void load(Matrix *M, ListUser *l, ListInventory *li, ListPerut *lp, boolean *sta
     char path[50];
     char user_path[100];
     char config_path[100];
-    const char *base_path = "/data/";
+    const char *base_path = "data/";
     const char *user_file = "user.csv";
     const char *config_file = "config.txt";
 
     snprintf(path, sizeof(path), "%s%s", base_path, folder_name);
     
-    if(!strcmp(path, base_path) || !isFolderExist(&path)){
-        if(!isFolderExist(&path)){
+    if(!strcmp(path, base_path) || !isFolderExist(path)){
+        if(!isFolderExist(path)){
             printf("folder %s tidak ditemukan", folder_name);
         }else if(!strcmp(path, base_path)){
             printf("Tidak ada nama folder yang diberikan!");
@@ -26,10 +26,10 @@ void load(Matrix *M, ListUser *l, ListInventory *li, ListPerut *lp, boolean *sta
         }
         *status = false;
     }
-    if(isFolderExist(&path)){
+    if(isFolderExist(path)){
         snprintf(user_path, sizeof(user_path), "%s%s", path, user_file);
-        readFileUser(l, &user_path);
+        readFileUser(l, user_path);
         snprintf(config_path, sizeof(config_path), "%s%s", path, config_file);
-        readConfig(M, li, lp, &config_path);
+        readConfig(M, li, lp, config_path);
     }
 }

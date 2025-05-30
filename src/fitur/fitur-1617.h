e70478d6f9155ca2506ea2f17ccece6f7f276652
Ishakpln <isaacnapz777999@gmail.com>
2025-05-30 12:45:14 +0700

    refactor: menghapus define MAX_NAME

--- src/fitur/fitur-1617.h
+++ src/fitur/fitur-1617.h
@@ -9,8 +9,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_NAME 100
-
 void minumObat(ListInventory *inventory, ListPerut *perut, MapObatPenyakit *mapObat, int userID, ListPenyakit *listpenyakit, ListUser listuser);
 //F16: Prosedur untuk meminum obat dari inventory ke perut
 void minumPenawar(ListInventory *inventory, ListPerut *perut, MapObatPenyakit *mapObat, int userID, ListPenyakit *listpenyakit, ListUser listuser);

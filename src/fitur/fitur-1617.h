50a46460ae0845ea18f8151d2f4061e8992e2f44
Ishakpln <isaacnapz777999@gmail.com>
2025-05-30 12:38:49 +0700

    feat: menghapus parameter user dan menambah parameter listuser

--- src/fitur/fitur-1617.h
+++ src/fitur/fitur-1617.h
@@ -6,12 +6,14 @@
 #include "../adt/map-obat-penyakit.h"
 #include "../adt/user-list.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_NAME 100
 
-void minumObat(ListInventory *inventory, ListPerut *perut, MapObatPenyakit *mapObat, int userID, User *user, ListPenyakit *listpenyakit);
+void minumObat(ListInventory *inventory, ListPerut *perut, MapObatPenyakit *mapObat, int userID, ListPenyakit *listpenyakit, ListUser listuser);
 //F16: Prosedur untuk meminum obat dari inventory ke perut
-void minumPenawar(ListInventory *inventory, ListPerut *perut, MapObatPenyakit *mapObat, int userID, User *user, ListPenyakit *listpenyakit);
+void minumPenawar(ListInventory *inventory, ListPerut *perut, MapObatPenyakit *mapObat, int userID, ListPenyakit *listpenyakit, ListUser listuser);
 //F17: Prosedur untuk mengeluarkan obat dari perut kembali ke inventory
 
 #endif
